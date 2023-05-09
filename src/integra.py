7dba500a207e20d9ca77b173e7da80af0f0f7246
Pavel <pavel.martynov@gmail.com>
2023-05-09 20:28:10 +0300

    Update integra.py to 200 LOC

--- src/integra.py
+++ src/integra.py
@@ -1,15 +1,13 @@
-import zmq, socket, time, json
+import zmq, socket, time, json, uuid
 from loguru import logger
-from uuid import uuid4
 from zeroconf import ServiceInfo, Zeroconf, ServiceBrowser, ServiceStateChange
 from functools import wraps
 from threading import Thread
 
-__version__ = "0.0.2"
 system_hostname: str = socket.gethostname()
 default_short_sleep_sec: float = 0.1
 default_poll_timeout_sec: int = 3
-default_wait_timeout: int = 10
+default_wait_timeout_sec: int = 10
 
 
 def myasync(func) -> None:
@@ -26,7 +24,7 @@ def myasync(func) -> None:
 
 class ServiceProxy(object):
 
-    def __init__(self, integra_instance, service_name) -> None:
+    def __init__(self, integra_instance: object, service_name: str) -> None:
         self.integra_instance: Integra = integra_instance
         self.service_name: str = service_name
         self.client_update_service_data()
@@ -69,15 +67,14 @@ class ServiceProxy(object):
         recv: dict = dict()
         poller = zmq.Poller()
         poller.register(self.socket, zmq.POLLIN)
-        if poller.poll(default_poll_timeout_sec * 1000):
+        if poller.poll(default_wait_timeout_sec):
             recv, error = self.socket.recv_pyobj(), recv.get("error", None)
             if error:
                 raise error
 
         else:
-            logger.warning(f"Service {self.service_name} lost.")
+            logger.warning(f"Service {self.service_name} lost, waiting for service.")
             self.integra_instance.forget_service(self.service_name)
-            logger.info(f"Waiting for service {self.service_name}.")
             while not self.service_name in self.integra_instance.dict_services:
                 time.sleep(default_poll_timeout_sec)
             self.client_update_service_data()  # This is service recovery
@@ -86,18 +83,17 @@ class ServiceProxy(object):
 
 class Integra(object):
 
-    def __init__(self, zmq_port: int = 0, local_only=False, debug=False) -> None:
+    def __init__(self, zmq_port: int = 0, local_only=False) -> None:
         with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-            s.settimeout(0)
-            s.connect(("10.254.254.254", 1))  # on error,
-            self.ip: str = s.getsockname()[0]  # we don't mute it.
+            s.connect(("10.254.254.254", 1))  #s.settimeout(0)
+            self.ip: str = s.getsockname()[0]  # on error, we don't mute it.
         self.run: bool = True
         self.zeroconf: Zeroconf = Zeroconf()
         self.dict_objects: dict = dict()  # What I offer
         self.dict_services: dict = dict()  # What do neighbours offer
         self.zmq_addr: str = "127.0.0.1" if local_only else "*"
         self.zmq_port = zmq_port
-        self.uuid: str = f"{uuid4()}"
+        self.uuid: str = uuid.uuid4()
         self.desc: dict = dict({"uuid": self.uuid, "services": [], "ip": self.ip})
         self.context: zmq.Context = zmq.Context()
         self.socket: zmq.Context.socket = self.context.socket(zmq.REP)
@@ -117,27 +113,13 @@ class Integra(object):
         self.zmq_port = int(real_endpoint.split(":")[2])
         reply: dict = dict()
         while self.run:
-            try:
-                request: object = self.socket.recv_pyobj()
-            except zmq.error.ContextTerminated:
-                break  # It *will* be terminated on exit.
-            service, attr, args, kwargs = (
-                request["service"],
-                request["attr"],
-                request["args"],
-                request["kwargs"],
-            )
-
-            try:
-                service_obj: object = self.dict_objects[service]
-            except Exception as e:
-                reply["error"] = RuntimeError(f"Error {e}: Service object {service} missing.")
-
-            service_attr = getattr(service_obj, attr, None)
-            if not service_attr:
-                reply["error"] = AttributeError(f"No attribute {attr} in {service}.")
-            res = service_attr(*args, **kwargs) if callable(service_attr) else service_attr
-            reply["result"] = res
+            request: object = self.socket.recv_pyobj()
+            service, attr, args, kwargs = (request[key] for key in ["service", "attr", "args", "kwargs"])
+            service_obj: object = self.dict_objects.get(service, None)
+            service_attr: object = getattr(service_obj, attr, None)
+            reply["error"] = RuntimeError(f"Error: Service object {service} missing.") if not service_obj else None
+            reply["error"] = AttributeError(f"No attribute {attr} in {service}.") if not service_attr else None
+            reply["result"] = service_attr(*args, **kwargs) if callable(service_attr) else service_attr
             self.socket.send_pyobj(reply)
 
     def on_service_state_change(
@@ -165,17 +147,14 @@ class Integra(object):
         del self.dict_services[name]
 
     def service_info_to_dict(self, service_info: ServiceInfo) -> dict:
-        res: dict = {}
-        res["name"] = service_info.name
-        res["port"] = int(service_info.port)
-        res["server"] = service_info.server
+        res: dict = {key: getattr(service_info, key, None) for key in ["name", "port", "server"]}
         properties: dict = service_info.properties
         properties = dict({
             key.decode("utf-8"): value.decode("utf-8")
             for key, value in properties.items()
         })  # Properties transmitted in binary, we decode...
-        properties["services"] = json.loads(properties["services"].replace("'", '"'))
-        # This is not my fault, but zeroconf passes them this way ---------^^^
+        properties["services"] = json.loads(properties["services"].replace(
+            "'", '"'))  # Not my fault, but zeroconf passes them this way
         res.update(properties)
         return res
 
@@ -197,16 +176,11 @@ class Integra(object):
         logger.success(f"Serving '{service_name}' as {self.uuid}...")
 
     def get_service_proxy(self, service_name: str) -> ServiceProxy:
-        if not service_name in self.dict_services:
-            return None
-
         service_item = self.dict_services.get(service_name, None)
-        logger.info(f"Service '{service_name}' found as {service_item}")
-        return ServiceProxy(self, service_name)
+        return ServiceProxy(self, service_name) if service_item else None
 
-    def get_service_wait(self, service_name: str, timeout: int = default_wait_timeout) -> ServiceProxy:
-        time_waited: int = 0
-        res: ServiceProxy = None
+    def get_service_wait(self, service_name: str, timeout: int = default_wait_timeout_sec) -> ServiceProxy:
+        time_waited, res = 0, None
         logger.info(f"Waiting for service {service_name}, timeout={timeout}...")
         while not res and time_waited < timeout:
             res: ServiceProxy = self.get_service_proxy(service_name)
